0f548545949b5490088f07f95ce13e8b86e173f9
Maxim Nefedyev <nefedev.m@gmail.com>
2024-01-30 16:47:52 +0300

    Delete pve-telmate directory

--- pve-telmate/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "vm_name" {
-  description = "The current VM name"
-  value       = proxmox_vm_qemu.deploy_vm.*.name
-}
-output "vm_ip_address" {
-  description = "The current IP address of the VM"
-  value       = proxmox_vm_qemu.deploy_vm.*.default_ipv4_address
-}
