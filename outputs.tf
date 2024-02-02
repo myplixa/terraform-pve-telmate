5132b6693397b0eb18b50b32e21ee91ad41433a7
Maxim Nefedyev <nefedev.m@gmail.com>
2024-02-02 15:14:46 +0300

    Added new output parameter "vm_info" in the format "key, values"

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,9 @@ output "vm_ip_address" {
   description = "The current IP address of the VM"
   value       = proxmox_vm_qemu.deploy_vm.*.default_ipv4_address
 }
+output "vm_info" {
+  value = {
+    for i, vname in proxmox_vm_qemu.deploy_vm.*.name:
+    vname => proxmox_vm_qemu.deploy_vm.*.default_ipv4_address[i]
+  }
+}
